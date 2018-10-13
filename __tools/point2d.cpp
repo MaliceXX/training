b7943e8ea8c23af74037c1e3088d183ef6637104
Tuan Hoang Nguyen <34983544+nth842002@users.noreply.github.com>
2018-10-13 17:25:59 +0700

    Create point2d.cpp

---
+++ __tools/point2d.cpp
@@ -0,0 +1,35 @@
+template <typename T>
+struct point2d {
+  typedef T ftype;
+
+  ftype x;
+  ftype y;
+
+  point2d() { }
+
+  point2d(const ftype& _x, const ftype& _y) : x(_x), y(_y) { }
+
+  point2d& operator+=(const point2d& p) {
+    x += p.x; 
+    y += p.y;
+    return *this;
+  }
+
+  point2d& operator-=(const point2d& p) {
+    x -= p.x; 
+    y -= p.y;
+    return *this;
+  }
+
+  point2d& operator*=(ftype a) {
+    x *= a; 
+    y *= a;
+    return* this;
+  }
+
+  point2d& operator/=(ftype a) {
+    x /= a;
+    y /= a;
+    return *this;
+  }
+};
