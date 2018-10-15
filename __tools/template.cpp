2bafb74d2f81e6659d05e461db152d3c4d2276a8
Tuan Hoang Nguyen <34983544+nth842002@users.noreply.github.com>
2018-10-15 22:14:57 +0700

    Create template.cpp

---
+++ __tools/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define eprintf(...) 42
+#endif
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  
+}
